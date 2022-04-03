3c4e714c7b91b58a4d1199cd3b0649fdf37a5986
Quynh Hoang <htnquynh@users.noreply.github.com>
2022-04-03 23:23:55 +0700

    feat: add responsive for why-pick section

--- src/components/WhyPick.vue
+++ src/components/WhyPick.vue
@@ -1,33 +1,71 @@
 <template>
-  <section class="py-16">
+  <section class="py-24 bg-[#ECEDFC]">
     <div class="container">
-      <div class="flex">
-        <div class="w-6/12">
-          <h2 class="text-6xl font-black">
+      <div class="flex flex-col items-center gap-8 md:flex-row">
+        <div class="md:w-6/12 md:order-2 relative">
+          <img
+            src="/assets/img/why-pick-illustration.png"
+            alt="Why pick illustration"
+            class="transform scale-125 origin-bottom md:translate-x-8"
+          />
+
+          <img
+            src="/assets/img/why-pick-decor-3.png"
+            alt="Feature"
+            class="absolute -top-16 -right-8"
+          />
+
+          <img
+            src="/assets/img/why-pick-decor-6.png"
+            alt="Feature"
+            class="absolute bottom-8 -left-8"
+          />
+        </div>
+        <div class="md:w-6/12 md:order-1">
+          <h2 class="text-4xl lg:text-6xl 2xl:text-80px font-black relative">
             Why Pick <span class="text-deep-blue-500">Hoomi</span> For Your
             Insurance <span class="text-deep-blue-500">Needs?</span>
+
+            <img
+              src="/assets/img/why-pick-decor-1.png"
+              alt="Feature"
+              class="absolute -top-16 -left-8"
+            />
+            <img
+              src="/assets/img/why-pick-decor-2.png"
+              alt="Feature"
+              class="absolute -top-8 -right-4"
+            />
+            <img
+              src="/assets/img/why-pick-decor-4.png"
+              alt="Feature"
+              class="absolute -bottom-4 right-16"
+            />
           </h2>
-          <p class="pt-6 text-xl opacity-80">
+          <p class="pt-6 text-lg md:text-xl opacity-80">
             Adding to your family means adding to your financial needs. We're
             here to guide you as you work to secure the future of your loved
             ones.
           </p>
-          <div class="pt-12">
-            <a
-              href="#"
-              class="inline-block uppercase bg-tomato-500 text-white px-8 py-3 rounded-5px"
-            >
-              Learn More
-            </a>
+          <div class="pt-12 relative">
+            <ButtonSolid>Learn More</ButtonSolid>
+            <img
+              src="/assets/img/why-pick-decor-5.png"
+              alt="Feature"
+              class="absolute bottom-2 right-32"
+            />
           </div>
-        </div>
-        <div class="w-6/12">
-          <img
-            src="/assets/img/why-pick-illustration.png"
-            alt="Why pick illustration"
-          />
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script>
+import ButtonSolid from "./buttons/ButtonSolid.vue";
+export default {
+  components: {
+    ButtonSolid,
+  },
+};
+</script>
---
+++ src/components/buttons/ButtonSolid.vue
@@ -0,0 +1,8 @@
+<template>
+  <a
+    href="#"
+    class="inline-block uppercase bg-tomato-500 text-white px-8 py-3 rounded-5px"
+  >
+    <slot></slot>
+  </a>
+</template>
