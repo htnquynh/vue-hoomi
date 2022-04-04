d44ba8845408759e407020ad3a30254196cfbf67
Quynh Hoang <htnquynh@users.noreply.github.com>
2022-04-04 15:03:04 +0700

    feat: add responsive for banner bottom section

--- src/components/BannerBottom.vue
+++ src/components/BannerBottom.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="">
-    <div class="py-28 pb-16 bg-deep-blue-500 text-white relative">
-      <div class="w-max mx-auto text-center z-10 relative">
-        <h2 class="text-6xl font-black max-w-3xl mx-auto">
+    <div class="py-28 pb-16 bg-deep-blue-500 text-center text-white relative">
+      <div class="inline-block mx-auto text-center z-10 relative">
+        <h2 class="text-4xl lg:text-6xl font-black max-w-3xl mx-auto">
           Let's Get Started Your Insurance now
         </h2>
         <p class="pt-8 max-w-xl mx-auto text-xl">
@@ -21,40 +21,40 @@
         <img
           src="/assets/img/banner-bottom-detail-1.png"
           alt="Banner illustration"
-          class="absolute -top-8 -left-4"
+          class="hidden lg:block lg:absolute lg:-top-8 lg:-left-4"
         />
         <img
           src="/assets/img/banner-bottom-detail-2.png"
           alt="Banner illustration"
-          class="absolute -bottom-10 left-1/5"
+          class="hidden lg:block lg:absolute lg:-bottom-10 lg:left-1/5"
         />
         <img
           src="/assets/img/banner-bottom-detail-3.png"
           alt="Banner illustration"
-          class="absolute -bottom-2 right-24"
+          class="hidden lg:block lg:absolute lg:-bottom-2 lg:right-24"
         />
       </div>
 
       <img
         src="/assets/img/banner-bottom-3.png"
         alt="Banner illustration"
-        class="absolute top-0 left-0"
+        class="hidden lg:block lg:absolute lg:top-0 lg:left-0"
       />
       <img
         src="/assets/img/banner-bottom-4.png"
         alt="Banner illustration"
-        class="absolute top-0 right-0"
+        class="hidden lg:block lg:absolute lg:top-0 lg:right-0"
       />
 
       <img
         src="/assets/img/banner-bottom-1.png"
         alt="Banner illustration"
-        class="absolute bottom-0 left-8 transform scale-75 origin-bottom-left"
+        class="hidden lg:block lg:absolute lg:bottom-0 lg:left-8 lg:transform lg:scale-75 lg:origin-bottom-left"
       />
       <img
         src="/assets/img/banner-bottom-2.png"
         alt="Banner illustration"
-        class="absolute bottom-0 right-8 transform scale-75 origin-bottom-right"
+        class="hidden lg:block lg:absolute lg:bottom-0 lg:right-8 lg:transform lg:scale-75 lg:origin-bottom-right"
       />
     </div>
   </section>
